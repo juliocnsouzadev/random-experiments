b5366bcc732369bd1807db4f3056077719aad3a9
Julio Souza <juliocnsouzadev@gmail.com>
2023-04-27 14:22:32 +0100

    refactor: deal with parentesis in the regex (#7)

--- strings/gen_str_from_regex.go
+++ strings/gen_str_from_regex.go
@@ -1,42 +1,70 @@
 package main
 
 import (
-    "math/rand"
-    "regexp"
-    "time"
+	"math/rand"
+	"regexp"
+	"strings"
+	"time"
 )
 
 // GenerateRandomStringFromRegex generates a random string from a regular expression
 func GenerateRandomStringFromRegex(regex string) (string, error) {
-    rand.Seed(time.Now().UnixNano())
+	rand.Seed(time.Now().UnixNano())
 
-    r, err := regexp.Compile(regex)
-    if err != nil {
-        return "", err
-    }
+	r, err := regexp.Compile(regex)
+	if err != nil {
+		return "", err
+	}
 
-    var result []byte
-    for _, c := range r.String() {
-        switch c {
-        case '[':
-            // handle character class
-            i := 1
-            for r.String()[i] != ']' {
-                i++
-            }
-            class := r.String()[2:i]\n\t\t	char := class[rand.Intn(len(class))]
-            result = append(result, byte(char))
-            r = regexp.MustCompile(r.String()[i+1:])
-        case '\\':
-            // handle escaped characters
-            result = append(result, byte(r.String()[1]))
-            r = regexp.MustCompile(r.String()[2:])
-        default:
-            // handle normal characters
-            result = append(result, byte(c))
-            r = regexp.MustCompile(r.String()[1:])
-        }
-    }
+	var result []byte
+	expr := r.String()
 
-    return string(result), nil
+	for len(expr) > 0 {
+		switch expr[0] {
+		case '[':
+			// handle character class
+			i := 1
+			for expr[i] != ']' {
+				i++
+			}
+			class := expr[2:i]
+			char := class[rand.Intn(len(class))]
+			result = append(result, byte(char))
+			expr = expr[i+1:]
+
+		case '\\':
+			// handle escaped characters
+			result = append(result, byte(expr[1]))
+			expr = expr[2:]
+
+		case '(':
+			// handle capturing groups
+			i := strings.IndexByte(expr, ')')
+			if i == -1 {
+				return "", regexp.ErrMissingParen
+			}
+			group := expr[1:i]
+			subR, err := regexp.Compile(group)
+			if err != nil {
+				return "", err
+			}
+			subStr, err := GenerateRandomStringFromRegex(subR)
+			if err != nil {
+				return "", err
+			}
+			result = append(result, []byte(subStr)...)
+			expr = expr[i+1:]
+
+		case '|':
+			// handle alternation
+			return "", regexp.ErrInternalError
+
+		default:
+			// handle normal characters
+			result = append(result, byte(expr[0]))
+			expr = expr[1:]
+		}
+	}
+
+	return string(result), nil
 }
